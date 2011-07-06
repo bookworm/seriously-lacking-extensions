89bc86534c65a2be14c9a4356a43342606f3e78f
Ken Erickson <bookworm.productions@gmail.com>
2011-07-06 16:10:36 -0700

    add truncate string method

--- lib/string.rb
+++ lib/string.rb
@@ -1,5 +1,13 @@
-class String
+class String    
+  
   def possessive
     self + ('s' == self[-1,1] ? "'" : "'s")
+  end 
+  
+  # Truncates a string 
+  def truncate(length = 30, end_string = '…')
+    return self if self == nil
+    words = self.split()
+    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
   end
 end
